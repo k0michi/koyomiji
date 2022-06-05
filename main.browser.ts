03c55eebfa398fd21fced08c0290251601bba5f7
Komichi <k0michi@koyomi.co>
2022-06-05 17:46:09 +0900

    Utilize dynamic import

--- main.browser.ts
+++ main.browser.ts
@@ -1,15 +1,20 @@
-import katex from 'katex';
-import prismjs from 'prismjs';
 import './assets/styles.css';
 import 'prismjs/themes/prism-tomorrow.css'
 import 'katex/dist/katex.min.css'
 
-window.addEventListener('load', () => {
-  prismjs.highlightAll();
+window.addEventListener('load', async () => {
+  if (document.querySelector('code') != null) {
+    const prismjs = await import('prismjs');
+    prismjs.highlightAll();
+  }
 
   const mathElements = document.getElementsByClassName('math-block');
 
-  for (const mathElement of mathElements) {
-    katex.render(mathElement.textContent!, mathElement as any, { displayMode: true });
+  if (mathElements.length > 0) {
+    const katex = (await import('katex')).default;
+
+    for (const mathElement of mathElements) {
+      katex.render(mathElement.textContent!, mathElement as any, { displayMode: true });
+    }
   }
 });
