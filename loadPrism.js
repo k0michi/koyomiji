64ac75be8b8f38d5674265f4ce3e2eed58c694a5
Komichi <k0michi@koyomi.co>
2022-06-12 01:44:47 +0900

    Add /blog/11

---
+++ loadPrism.js
@@ -0,0 +1,5 @@
+export default async function () {
+  await import('prismjs/components/prism-typescript.js');
+  await import('prismjs/components/prism-jsx.js');
+  await import('prismjs/components/prism-tsx.js');
+}
--- main.browser.ts
+++ main.browser.ts
@@ -1,10 +1,12 @@
 import './assets/styles.css';
 import 'prismjs/themes/prism-tomorrow.css'
 import 'katex/dist/katex.min.css'
+import loadPrism from './loadPrism.js';
 
 window.addEventListener('load', async () => {
   if (document.querySelector('code') != null) {
     const prismjs = await import('prismjs');
+    await loadPrism();
     prismjs.highlightAll();
   }
 
