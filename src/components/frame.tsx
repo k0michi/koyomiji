c4c056811a9dd0601af5d8c4bb052b46229a9f1a
Komichi <k0michi@koyomi.co>
2022-06-27 23:26:08 +0900

    Remove redundant /

--- src/components/frame.tsx
+++ src/components/frame.tsx
@@ -5,8 +5,8 @@ export default function Frame(props: any) {
     <>
       <nav id="nav">
         <h1 id="site-logo"><a href="/"><img alt="曆路" id="logo-hr" /><img alt="曆路" id="logo-vt" /></a></h1>
-        <h2><a href="/knowledge/">備忘録</a></h2>
-        <h2><a href="/log/">雑記帳</a></h2>
+        <h2><a href="/knowledge">備忘録</a></h2>
+        <h2><a href="/log">雑記帳</a></h2>
       </nav>
       <div id="main-container">
         <main id="main">
