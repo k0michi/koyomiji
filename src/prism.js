9f0401b35aa5e5028d946beb5e0dd45cb4c88b83
Komichi <k0michi@koyomi.co>
2022-06-30 14:53:31 +0900

    Add /knowledge/development/d-hello

--- src/prism.js
+++ src/prism.js
@@ -4,6 +4,7 @@ import 'prismjs/components/prism-jsx.js';
 import 'prismjs/components/prism-tsx.js';
 import 'prismjs/components/prism-python.js';
 import 'prismjs/components/prism-bash.js';
+import 'prismjs/components/prism-d.js';
 
 export function highlightAll() {
   prismjs.highlightAll();
