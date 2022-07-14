22935ce6d3c0aa95cd63b0d9128847eab8cd1708
Komichi <k0michi@koyomi.co>
2022-07-15 00:25:06 +0900

    Change my name

--- src/components/about-page.tsx
+++ src/components/about-page.tsx
@@ -16,12 +16,12 @@ export default function AboutPage() {
         <meta name="twitter:site" content="@k0michi" />
       </Nano.Helmet>
       <Frame>
-        <p>管理人: 小路はじめ (<a href="https://twitter.com/k0michi">@k0michi</a>)</p>
+        <p>管理人: 小路よみ (<a href="https://twitter.com/k0michi">@k0michi</a>)</p>
         <p>情報技術や英語、数学を学んでいます。</p>
         <p>最近の趣味は読書です。シャーロックホームズなど、推理小説が好きです。</p>
         <p>主にWebサイト、デスクトップアプリ、ゲームを開発しています。</p>
         <p>お絵描き練習中です。</p>
-        <p>Administrator: Hajime Komichi (<a href="https://twitter.com/k0michi">@k0michi</a>)</p>
+        <p>Administrator: Yomi Komichi (<a href="https://twitter.com/k0michi">@k0michi</a>)</p>
         <p>I'm a Japanese student and learning information technology, English and mathematics.</p>
         <p>My hobby is reading mysteries. I'm fond of Sherlock Holmes series.</p>
         <p>I mainly develop websites, desktop apps and games.</p>
