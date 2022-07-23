e2e398c5a3e88cfbef62c64c30b22fe660e2f1ae
Komichi <k0michi@koyomi.co>
2022-07-23 20:50:34 +0900

    Add license notice

--- src/components/about-page.tsx
+++ src/components/about-page.tsx
@@ -16,6 +16,7 @@ export default function AboutPage() {
         <meta name="twitter:site" content="@k0michi" />
       </Nano.Helmet>
       <Frame>
+        <h2>私について</h2>
         <p>管理人: 小路よみ (<a href="https://twitter.com/k0michi">@k0michi</a>)</p>
         <p>情報技術や英語、数学を学んでいます。</p>
         <p>最近の趣味は読書です。シャーロックホームズなど、推理小説が好きです。</p>
@@ -26,6 +27,9 @@ export default function AboutPage() {
         <p>My hobby is reading mysteries. I'm fond of Sherlock Holmes series.</p>
         <p>I mainly develop websites, desktop apps and games.</p>
         <p>I'm currently practicing drawing pictures.</p>
+        <h2>サイトについて</h2>
+        <p>特段の明記がない限り、サイト上のコンテンツは<a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>に準拠します。</p>
+        <p></p>
       </Frame>
     </>
   );
