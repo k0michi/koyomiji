18a0480e61c62672348e4deffd276201e7807090
Komichi <k0michi@koyomi.co>
2022-08-01 12:31:03 +0900

    Remove trailing <hr>

--- src/components/log-index-page.tsx
+++ src/components/log-index-page.tsx
@@ -8,6 +8,20 @@ interface Props {
   items: PostHead[];
 }
 
+function join(elements: JSX.Element[]) {
+  const newElements = [];
+
+  for (let i = 0; i < elements.length; i++) {
+    newElements.push(elements[i]);
+
+    if (i < elements.length - 1) {
+      newElements.push(<hr />);
+    }
+  }
+
+  return newElements;
+}
+
 export default function LogIndexPage(props: Props) {
   const url = `https://koyomiji.com/log`;
   props.items.sort((a, b) => b.id.localeCompare(a.id, undefined, { numeric: true }));
@@ -23,19 +37,16 @@ export default function LogIndexPage(props: Props) {
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@k0michi" />
       </Helmet>
-      {props.items.map(i =>
-        <>
-          <div className="summary">
-            <h2><a href={`/log/${i.id}`}>{i.title}</a></h2>
-            <div className="meta">
-              <div className="number">#{i.id}</div>
-              <div className="date"><div className="calender-icon"></div><div>{dateToString(new Date(i.created))}</div></div>
-            </div>
-            <p>{i.description}</p>
+      {join(props.items.map(i =>
+        <div className="summary">
+          <h2><a href={`/log/${i.id}`}>{i.title}</a></h2>
+          <div className="meta">
+            <div className="number">#{i.id}</div>
+            <div className="date"><div className="calender-icon"></div><div>{dateToString(new Date(i.created))}</div></div>
           </div>
-          <hr />
-        </>
-      )}
+          <p>{i.description}</p>
+        </div>
+      ))}
     </>
   );
 }
