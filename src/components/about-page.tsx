86ebf562e28c279d513ca6dd3e6f73be29cf745b
Komichi <k0michi@koyomi.co>
2022-08-10 16:53:22 +0900

    Update about page

--- src/components/about-page.tsx
+++ src/components/about-page.tsx
@@ -16,18 +16,20 @@ export default function AboutPage() {
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@k0michi" />
       </Helmet>
-      <h2>私について</h2>
-      <p>管理人: 小路よみ (<a href="https://twitter.com/k0michi">@k0michi</a>)</p>
+      <h2>管理人について</h2>
+      <p>管理人: Komichi* (<a href="https://twitter.com/k0michi">@k0michi</a>)</p>
       <p>情報技術や英語、数学を学んでいます。</p>
       <p>最近の趣味は読書です。シャーロックホームズなど、推理小説が好きです。</p>
       <p>主にWebサイト、デスクトップアプリ、ゲームを開発しています。</p>
       <p>お絵描き練習中です。</p>
-      <p>Administrator: Yomi Komichi (<a href="https://twitter.com/k0michi">@k0michi</a>)</p>
+      <p>Administrator: Komichi* (<a href="https://twitter.com/k0michi">@k0michi</a>)</p>
       <p>I'm a Japanese student and learning information technology, English and mathematics.</p>
       <p>My hobby is reading mysteries. I'm fond of Sherlock Holmes series.</p>
       <p>I mainly develop websites, desktop apps and games.</p>
       <p>I'm currently practicing drawing pictures.</p>
       <h2>サイトについて</h2>
+      <p></p>
+      <p><ruby>曆路<rp></rp><rt>こよみじ</rt><rp></rp></ruby>喫茶館といいます。</p>
       <p>特段の明記がない限り、サイト上のコンテンツは<a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>に準拠します。</p>
     </>
   );
