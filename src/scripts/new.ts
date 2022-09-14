ba0a266bffc557b3da75b0febd998169739e7e9b
Komichi <k0michi@koyomi.co>
2022-09-14 22:55:14 +0900

    Fix import

--- src/scripts/new.ts
+++ src/scripts/new.ts
@@ -1,7 +1,7 @@
 import * as path from "path";
 import * as fs from "fs/promises";
 import crypto from "crypto";
-import { toISOStringJST } from "../date-format";
+import { toISOStringJST } from "../date-format.js";
 
 (async () => {
   let pathToCreate = process.argv[2];
