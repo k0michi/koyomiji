e7546a0de45ef982ecc0d8f4151d058393cea704
Komichi <k0michi@koyomi.co>
2022-09-16 01:00:08 +0900

    Update about page

--- src/pages/about-page.tsx
+++ src/pages/about-page.tsx
@@ -9,26 +9,31 @@ export default function AboutPage() {
 
   return (
     <>
-      <Head url={url} title="About" description="私は一体誰か。" />
+      <Head url={url} title="About" description="このサイトは一体何か。" />
       <header>
         <h1>About</h1>
-        <div className="meta">私は一体誰か。</div>
+        <div className="meta">このサイトは一体何か。</div>
       </header>
       <div id="body">
         <h2>サイトについて</h2>
-        <p>初めまして。このサイト <ruby>曆路<rp></rp><rt>こよみじ</rt><rp></rp></ruby>喫茶館 を運営している Komichi と申します。このサイトは、私が気まぐれに勉強したことを書いたり、日記を書いたり、創作したりするための場所です。プログラミングや英語、数学に関する内容が多いですが、これらに限らず私の興味のあることを取り扱っていきます。</p>
-        <p>特段の明記がない限り、サイト上のコンテンツは<Link href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</Link>に準拠します。</p>
-        <h2>サイト内のページについて</h2>
+        <p>当サイト <ruby>曆路<rp></rp><rt>こよみじ</rt><rp></rp></ruby>喫茶館 を運営している Komichi と申します。このサイトは、私が気まぐれに勉強したことを書いたり、日記を書いたり、創作したりするための場所です。プログラミングや英語、数学に関する内容が多いですが、これらに限らず私の興味のあることを取り扱っていきます。</p>
+        <p>特段の明記がない限り、サイト上のコンテンツは<Link href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</Link>に準拠することとします。</p>
+        <h2>ページについて</h2>
         <ul>
           <li><Link href="/project">Projects</Link>には、私が開発している(していた)いくつかのソフトウェアの紹介があります。</li>
           <li><Link href="/log">Logs</Link>は、私のブログです。日記やエッセイなどを、形式に囚われず自由に書いています。</li>
           <li><Link href="/knowledge">Knowledge</Link>は、私が学んだことの備忘録です。</li>
           <li><Link href="/dictionary">Dictionary</Link>は、私が出逢った英単語をまとめた辞書です。</li>
         </ul>
-        <h2>サイトの技術構成について</h2>
-        <p>このサイトは、既存の技術スタックに極力依存しないことを目標として作成されています。</p>
-        <p>全てのページは、Node.js上で動く自作の静的サイトジェネレータを使用して生成されています。フロントエンドフレームワークにはReactを採用し、モジュールバンドラにはViteを使用しています。サイトはVercel上でビルドされ、ホストされています。</p>
-        <p>サイト上のテキストは、XMLベースの独自フォーマットで記述されています。</p>
+        <h2>フィードについて</h2>
+        <p>サイトのAtomフィードは<Link href="https://koyomiji.com/feed.xml">https://koyomiji.com/feed.xml</Link>にあります。このサイトの更新情報を、お好みのRSSリーダーで取得することが可能です。</p>
+        <h2>お問い合せ</h2>
+        <p><Link href="mailto:k0michi@koyomi.co">k0michi@koyomi.co</Link>まで。何でも大歓迎です。</p>
+        <h2>技術構成について</h2>
+        <p>当サイトの全てのページは、TypeScriptで書かれた自作の静的サイトジェネレータを使用して生成されています。フロントエンドフレームワークにはReactを採用し、モジュールバンドラにはViteを使用しています。サイトはVercel上でビルド、ホストされています。</p>
+        <p>サイト上のテキストは、XMLベースのフォーマットで記述されています。</p>
+        <h2>運営方針について</h2>
+        <p>レトロモダンなWebサイト作りを目標としています。</p>
       </div>
     </>
   );
