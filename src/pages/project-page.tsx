2ec54323707f35059e3ac852bfd79ef10d81b0a3
Komichi <k0michi@koyomi.co>
2022-10-04 17:23:49 +0900

    Update project page

--- src/pages/project-page.tsx
+++ src/pages/project-page.tsx
@@ -15,10 +15,12 @@ export default function ProjectPage() {
         <div className="meta">開発したもの。</div>
       </header>
       <div id="body">
+        <h2><Link href="https://github.com/k0michi/kotonoha">Kotonoha</Link></h2>
+        <p>Ankiクローン。単語学習アプリ。</p>
         <h2><Link href="https://github.com/k0michi/isomorphic-dom">@k0michi/isomorphic-dom</Link></h2>
         <p>jsdomとネイティブのDOM APIへのプロキシライブラリ。</p>
         <h2><Link href="https://k0michi.github.io/chronograph/">Chronograph</Link></h2>
-        <p>時間を記録、可視化するためのツール。ボタンをクリックして現在時刻を記録し、時刻を一覧表示することができます。各時刻にはメモを残すことも可能です。</p>
+        <p>時間を記録、可視化するためのツール。ボタンをクリックして現在時刻を記録し、時刻を一覧表示することができる。各時刻にはメモを残すことが可能。</p>
         <h2><Link href="https://k0michi.github.io/toolbox/">Toolbox</Link></h2>
         <p>個人的に使うためのWebツール。</p>
         <h2><Link href="https://github.com/k0michi/kyoka">Kyōka</Link></h2>
