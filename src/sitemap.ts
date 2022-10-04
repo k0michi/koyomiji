990714854a3c69c2979d0d51bfbf4f1629d6e039
Komichi <k0michi@koyomi.co>
2022-10-04 13:22:14 +0900

    Fix

--- src/sitemap.ts
+++ src/sitemap.ts
@@ -1,5 +1,5 @@
 import window from '@k0michi/isomorphic-dom';
-import { newElementCreator } from './utils.js';
+import { newElementCreator } from './xml.js';
 
 export interface MapURL {
   loc: string;
