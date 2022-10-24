dbcb9e2cb879a8f463dde16f47db054dcd99dc30
Komichi <k0michi@koyomi.co>
2022-10-24 22:14:45 +0900

    Fix scroll

--- src/components/calender-graph.tsx
+++ src/components/calender-graph.tsx
@@ -31,52 +31,54 @@ export default function CalenderGraph(props: CalenderGraphProps) {
   const endOffset = differenceInCalendarDays(nextSaturday(props.end), props.begin);
 
   return (
-    <table className="calender-graph">
-      <tbody>
-        {
-          range(beginOffset - 7, endOffset + 1, 7).map(j => {
-            const lastDay = add(props.begin, { days: j + 6 });
-
-            if (lastDay.getDate() <= 7) {
-              return <td className='cell' key={j}>{format(lastDay, 'LLL')}</td>;
-            }
-
-            return <td className='cell' key={j} />;
-          })
-        }
-        {
-          range(0, 7, 1).map(i => <tr key={i}> {
+    <div className="calender-graph-wrapper">
+      <table className="calender-graph">
+        <tbody>
+          {
             range(beginOffset - 7, endOffset + 1, 7).map(j => {
-              const index = i + j;
+              const lastDay = add(props.begin, { days: j + 6 });
 
-              if (j == beginOffset - 7) {
-                if (i % 3 == 0) {
-                  const day = add(props.begin, { days: index });
-                  return <td className='cell week' key={j}>{format(day, 'eee')}</td>;
-                } else {
-                  return <td className='cell week' key={j} />;
-                }
+              if (lastDay.getDate() <= 7) {
+                return <td className='cell' key={j}>{format(lastDay, 'LLL')}</td>;
               }
 
-              let color;
+              return <td className='cell' key={j} />;
+            })
+          }
+          {
+            range(0, 7, 1).map(i => <tr key={i}> {
+              range(beginOffset - 7, endOffset + 1, 7).map(j => {
+                const index = i + j;
 
-              if (index >= 0 && index < counted.length) {
-                const count = counted[index];
-
-                if (count > 0) {
-                  color = chroma.scale('YlGn').domain([0, 10])(count + 1);
-                  return <td className='cell filled' key={j} style={{ backgroundColor: color.css() }} />;
-                } else {
-                  return <td className='cell filled' key={j} />;
+                if (j == beginOffset - 7) {
+                  if (i % 3 == 0) {
+                    const day = add(props.begin, { days: index });
+                    return <td className='cell week' key={j}>{format(day, 'eee')}</td>;
+                  } else {
+                    return <td className='cell week' key={j} />;
+                  }
                 }
-              } else {
-                return <td className='cell' key={j} />;
-              }
-            })}
-          </tr>)
-        }
-      </tbody>
-    </table>
+
+                let color;
+
+                if (index >= 0 && index < counted.length) {
+                  const count = counted[index];
+
+                  if (count > 0) {
+                    color = chroma.scale('YlGn').domain([0, 10])(count + 1);
+                    return <td className='cell filled' key={j} style={{ backgroundColor: color.css() }} />;
+                  } else {
+                    return <td className='cell filled' key={j} />;
+                  }
+                } else {
+                  return <td className='cell' key={j} />;
+                }
+              })}
+            </tr>)
+          }
+        </tbody>
+      </table>
+    </div>
   );
 }
 
