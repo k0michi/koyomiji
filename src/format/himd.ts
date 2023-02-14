142ac86c7fd022a7cd329ff59262f17fe643c587
Komichi <k0michi@koyomi.co>
2023-02-14 15:18:12 +0900

    Make use of 'himd'

--- src/format/himd.ts
+++ src/format/himd.ts
@@ -1,4 +1,4 @@
-import * as hipt from 'hipt';
+import * as himd from 'himd';
 
 import * as markdown from './markdown.js';
 import { toXml } from 'xast-util-to-xml';
@@ -10,28 +10,5 @@ export function toXML(source: string) {
 }
 
 export function toMarkdown(source: string) {
-  const tree = hipt.parse(source, { deepenIf: value => value.startsWith('#') });
-
-  if (tree.children.length != 1) {
-    throw new Error('Tree must have only one child');
-  }
-
-  const flattened = flatten(tree.children[0]);
-  return flattened;
-}
-
-function flatten(node: hipt.Node, depth = 0): string {
-  let content = '#'.repeat(depth) + node.value! + '\n';
-
-  for (const c of node.children) {
-    if (c.value!.startsWith('#')) {
-      content += '\n<section>\n\n';
-      content += flatten(c, depth + 1);
-      content += '\n</section>\n\n';
-    } else {
-      content += c.value! + '\n';
-    }
-  }
-
-  return content;
+  return himd.compile(source);
 }
