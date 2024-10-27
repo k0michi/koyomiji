ee1865207b28d10f0b7e37359a07c8f1acfcabf9
Komichi <k0michi@koyomi.co>
2024-10-27 21:53:51 +0900

    Overwrite RR7 template

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
-import * as path from 'path';
-import license from 'rollup-plugin-license';
-import { defineConfig } from 'vite';
+import { reactRouter } from "@react-router/dev/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [
-    license({
-      banner: {
-        content: `Please see /license.txt for license information`,
-        commentStyle: 'ignored',
-      },
-      thirdParty: {
-        includePrivate: false,
-        output: {
-          file: path.join('dist', 'license.txt')
-        },
-      },
-    })
-  ]
+  plugins: [reactRouter(), tsconfigPaths()],
 });
