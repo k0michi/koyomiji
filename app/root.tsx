9fef781f5a943e14eece0db1924e48213e35cb83
Komichi <k0michi@koyomi.co>
2024-10-28 11:44:25 +0900

    Handle experiments

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,17 @@
-import { isRouteErrorResponse, Links, Meta, Outlet, Scripts, ScrollRestoration, useRouteError } from "react-router";
+import { isRouteErrorResponse, Links, Meta, Outlet, Scripts, ScrollRestoration, useMatches, useRouteError } from "react-router";
 import type { LinksFunction } from "react-router";
 
 import "./app.css";
 
+// type Handle = () => { lang: string };
+
 export default function App() {
+  // const matches = useMatches();
+  // const lang = matches.map(m => (m.handle as (Handle | undefined))?.()?.lang).reduceRight((previous, current) => current ?? previous);
+
   return (
-    <html lang="en">
+    // <html lang={lang ?? "en"}>
+    <html lang="ja">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
