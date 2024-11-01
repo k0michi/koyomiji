44c47555db15176915b56bec1bf487ad99ac9415
Komichi <k0michi@koyomi.co>
2024-11-02 00:28:30 +0900

    Add test

---
+++ tests/XMLNodeWriter.test.ts
@@ -0,0 +1,73 @@
+import { describe, expect, test } from '@jest/globals';
+import XMLDocumentReader from '../lib/visitor/XMLDocumentReader';
+import XMLDocumentWriter from '../lib/visitor/XMLDocumentWriter';
+
+function roundtrip(xml: string) {
+  const reader = new XMLDocumentReader(xml);
+  const writer = new XMLDocumentWriter();
+  writer.setEmitXMLDecl(false);
+  reader.accept(writer);
+  expect(writer.toString()).toBe(xml);
+}
+
+describe('XMLNodeWriter', () => {
+  test('Element 1', () => {
+    roundtrip(`<a/>`);
+  });
+
+  test('Attribute 1', () => {
+    roundtrip(`<a href="https://koyomiji.com/"/>`);
+  });
+
+  test('Attribute 2', () => {
+    roundtrip(`<a a="1" b="2" c="3"/>`);
+  });
+
+  test('Nest 1', () => {
+    roundtrip(`<a><b/></a>`);
+  });
+
+  test('Nest 2', () => {
+    roundtrip(`<a><b/><c/></a>`);
+  });
+
+  test('Text 1', () => {
+    roundtrip(`<a>abc</a>`);
+  });
+
+  test('CDATA 1', () => {
+    roundtrip(`<a><![CDATA[abc<>&"']]></a>`);
+  });
+
+  test('Processing Instruction 1', () => {
+    roundtrip(`<?abc ?><a/>`);
+  });
+
+  test('Processing Instruction 2', () => {
+    roundtrip(`<?abc e="f"?><a/>`);
+  });
+
+  test('Comment 1', () => {
+    roundtrip(`<a><!--comment--></a>`);
+  });
+
+  test('Comment 2', () => {
+    roundtrip(`<a/><!--a-->`);
+  });
+
+  test('Comment 3', () => {
+    roundtrip(`<!--b--><a/><!--a-->`);
+  });
+
+  test('Document Type 1', () => {
+    roundtrip(`<!DOCTYPE greeting SYSTEM "hello.dtd"><greeting>Hello, world!</greeting>`);
+  });
+
+  test('XML Decl 1', () => {
+    const reader = new XMLDocumentReader(`<a href="https://koyomiji.com/"/>`);
+    const writer = new XMLDocumentWriter();
+    writer.setEmitXMLDecl(true);
+    reader.accept(writer);
+    expect(writer.toString()).toBe(`<?xml version="1.0" encoding="UTF-8"?><a href="https://koyomiji.com/"/>`);
+  });
+});
