d9a9ed75d16579d65937ecfeee28a37a71d1707d
k0michi <k0michi@koyomi.co>
2025-01-23 22:21:20 +0900

    Fix parseAsReact

--- app/KTMLHelper.ts
+++ app/KTMLHelper.ts
@@ -1,12 +1,12 @@
 import KTMLReactTransformer from "lib/KTMLReactTransformer";
 import ReactFragmentWriter from "lib/visitor/ReactFragmentWriter";
-import XMLFragmentReader from "lib/visitor/XMLFragmentReader";
+import XMLFragmentParser from "lib/visitor/XMLFragmentParser";
 
 export default class KTMLHelper {
   static parseAsReact(xml: string) {
     const writer = new ReactFragmentWriter();
     const transformer = new KTMLReactTransformer(writer);
-    const reader = XMLFragmentReader.fromString(xml);
+    const reader = new XMLFragmentParser(xml);
     reader.accept(transformer);
     return writer.toReactNode();
   }
