fc97d669a1fceaadc133a50fc3d87c65570de913
k0michi <k0michi@koyomi.co>
2025-04-01 16:56:03 +0900

    Better touch.ts

--- scripts/touch.ts
+++ scripts/touch.ts
@@ -22,18 +22,43 @@ function editModified($document: Document) {
   $modified.textContent = toISOStringJST(now);
 }
 
+function blankPage(date: Date) {
+  const id = crypto.randomUUID();
+
+  return `<?xml version="1.0" encoding="UTF-8"?>
+<ktml version="0.1">
+
+<head>
+  <title>Untitled</title>
+  <id>${id}</id>
+  <created>${toISOStringJST(date)}</created>
+  <modified>${toISOStringJST(date)}</modified>
+</head>
+
+<body>
+</body>
+
+</ktml>`;
+}
+
 (async () => {
   let pathsToTouch = process.argv.slice(2);
 
   for (let pathToTouch of pathsToTouch) {
+    await fs.mkdir(pathToTouch, { recursive: true });
     pathToTouch = path.join(pathToTouch, 'index.ktml');
 
     const parser = new window.DOMParser();
-    const content = await FSHelper.readFileUTF8(pathToTouch);
-    const $document = parser.parseFromString(content, 'text/xml');
-    editModified($document);
-    const serializer = new window.XMLSerializer();
-    const modifiedContent = serializer.serializeToString($document);
-    await fs.writeFile(pathToTouch, modifiedContent);
+
+    try {
+      const content = await FSHelper.readFileUTF8(pathToTouch);
+      const $document = parser.parseFromString(content, 'text/xml');
+      editModified($document);
+      const serializer = new window.XMLSerializer();
+      const modifiedContent = serializer.serializeToString($document);
+      await fs.writeFile(pathToTouch, modifiedContent);
+    } catch (e) {
+      await fs.writeFile(pathToTouch, blankPage(new Date()));
+    }
   }
 })();
